df2c979e1fb87e1080ab7157cb53037a6c935148
Md. Sabuj Sarker <md.sabuj.sarker@gmail.com>
2020-05-07 11:30:37 +0600

    docker-compose.yaml and run.sh

---
+++ run.sh
@@ -0,0 +1,9 @@
+if [ ! -d "./wp" ]; then
+  mkdir ./wp
+  echo "wp directory created."
+else
+  echo "wp directory already exists."
+fi
+USER=$(id -u):$(id -g)
+echo "User for docker compose: $USER."
+docker-compose "$@"
